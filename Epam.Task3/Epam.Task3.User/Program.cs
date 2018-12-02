965de2ad61ff14219dbac8c6d384534bc846ea3f
Kirill Shubin <kshubin@gmail.com>
2018-12-02 23:24:10 +0300

    Task3.User added

---
+++ Epam.Task3/Epam.Task3.User/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Userr petya = new Userr("Козлов", "Петр", "Иванович", new DateTime(2000, 12, 01));
+            petya.UserInfo();
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.User/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.User
+{
+    public class Userr
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - Birthdate.Year;
+                if (Birthdate > now.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+
+        }
+        public Userr(string Surname, string Name, string Patronymic, DateTime Birthdate)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Patronymic = Patronymic;
+            this.Birthdate = Birthdate;
+        }
+
+        public virtual void UserInfo()
+        {
+            Console.WriteLine("Фамилия: {0}\nИмя: {1}\nОтчество: {2}\nДата рождения: {3}\nВозраст: {4}", Surname, Name, Patronymic, Birthdate, Age);
+        }
+    }
+}
